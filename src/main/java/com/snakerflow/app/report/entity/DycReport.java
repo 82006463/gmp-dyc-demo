71eb25a442ec9b5324b27833ddbdadd81d2db1d0
82006463 <82006463@163.com>
2017-03-11 15:17:18 +0800

    aaaaa

--- src/main/java/com/snakerflow/app/report/entity/DycReport.java
+++ src/main/java/com/snakerflow/app/report/entity/DycReport.java
@@ -4,6 +4,7 @@ import com.snakerflow.common.entity.IdEntity;
 
 import javax.persistence.Entity;
 import javax.persistence.Table;
+import java.util.Date;
 
 /**
  * Created by Administrator on 2017/3/11.
@@ -11,5 +12,20 @@ import javax.persistence.Table;
 @Entity
 @Table(name = "dyc_report")
 public class DycReport extends IdEntity {
+
+    private String processType; //流程类型
+    private String processNo; //流程编号
+    private String processName; //流程名称
+
+    private Long deptId; //部门
+    private String level; //级别
+    private Date occurTime;//发现时间
+    private Date startTime; //流程开始时间
+    private Date endTime; //流程关闭时间
+
+    //备用的字段
+    private String backup1;
+    private String backup2;
+    private String extraJson; //流程额外的非键数据
 
 }
