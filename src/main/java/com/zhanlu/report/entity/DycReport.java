0642a84da248c5c0bbc3e879036f7ef261824f4e
82006463 <82006463@163.com>
2017-04-09 16:32:40 +0800

    aaa

--- src/main/java/com/zhanlu/report/entity/DycReport.java
+++ src/main/java/com/zhanlu/report/entity/DycReport.java
@@ -1,6 +1,6 @@
 package com.zhanlu.report.entity;
 
-import com.zhanlu.framework.common.entity.IdEntity;
+import com.zhanlu.framework.common.entity.CodeEntity;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.Column;
@@ -17,24 +17,53 @@ import java.util.Date;
  */
 @Entity
 @Table(name = "dyc_report")
-public class DycReport extends IdEntity {
+public class DycReport extends CodeEntity {
 
+    //流程通用属性
     private String processType; //流程类型
-    private String processNo; //流程编号
-    private String processName; //流程名称
-    private String processDesc;//描述
-
     private Long orgId; //部门
     private String orgName;
     private String level; //级别
-    private Date occurTime;//发现时间
-    private Date closeTime; //流程关闭时间
     private String occurPerson;//发现人
+    private Date occurTime;//发现时间
     private Date startTime; //流程开始时间
+    private Date closeTime; //流程关闭时间
     private Date endTime; //流程结束时间
 
-    private String materielCode;//物料编号
-    private String materielName;//物料名称
+    //备用属性：具体语义在伸缩表配置时定义
+    private String type1;//备用类型
+    private String type2;
+    private String str21;
+    private String str22;
+    private String str23;
+    private String str24;
+    private String str25;
+    private String str51;
+    private String str52;
+    private String str53;
+    private String str54;
+    private String str55;
+
+    private Integer int1;
+    private Integer int2;
+    private Integer int3;
+    private Integer int4;
+    private Integer int5;
+
+    private Long long1;
+    private Long long2;
+    private Long long3;
+
+    private Date date1;
+    private Date date2;
+    private Date date3;
+    private Date date4;
+    private Date date5;
+
+    private Date time1;
+    private Date time2;
+    private Date time3;
+
     private String extraJson; //流程额外的非键数据
 
     @Column(name = "process_type", length = 10)
@@ -44,24 +73,6 @@ public class DycReport extends IdEntity {
 
     public void setProcessType(String processType) {
         this.processType = processType;
-    }
-
-    @Column(name = "process_no", length = 50)
-    public String getProcessNo() {
-        return processNo;
-    }
-
-    public void setProcessNo(String processNo) {
-        this.processNo = processNo;
-    }
-
-    @Column(name = "process_name", length = 100)
-    public String getProcessName() {
-        return processName;
-    }
-
-    public void setProcessName(String processName) {
-        this.processName = processName;
     }
 
     @Column(name = "level", length = 20)
@@ -112,15 +123,6 @@ public class DycReport extends IdEntity {
         this.extraJson = extraJson;
     }
 
-    @Column(name = "process_desc", length = 200)
-    public String getProcessDesc() {
-        return processDesc;
-    }
-
-    public void setProcessDesc(String processDesc) {
-        this.processDesc = processDesc;
-    }
-
     @Column(name = "occur_person", length = 20)
     public String getOccurPerson() {
         return occurPerson;
@@ -128,24 +130,6 @@ public class DycReport extends IdEntity {
 
     public void setOccurPerson(String occurPerson) {
         this.occurPerson = occurPerson;
-    }
-
-    @Column(name = "materiel_code", length = 50)
-    public String getMaterielCode() {
-        return materielCode;
-    }
-
-    public void setMaterielCode(String materielCode) {
-        this.materielCode = materielCode;
-    }
-
-    @Column(name = "materiel_name", length = 100)
-    public String getMaterielName() {
-        return materielName;
-    }
-
-    public void setMaterielName(String materielName) {
-        this.materielName = materielName;
     }
 
     @Column(name = "close_time")
@@ -174,5 +158,265 @@ public class DycReport extends IdEntity {
 
     public void setOrgName(String orgName) {
         this.orgName = orgName;
+    }
+
+    @Column(name = "type1", length = 20)
+    public String getType1() {
+        return type1;
+    }
+
+    public void setType1(String type1) {
+        this.type1 = type1;
+    }
+
+    @Column(name = "type2", length = 20)
+    public String getType2() {
+        return type2;
+    }
+
+    public void setType2(String type2) {
+        this.type2 = type2;
+    }
+
+    @Column(name = "int1")
+    public Integer getInt1() {
+        return int1;
+    }
+
+    public void setInt1(Integer int1) {
+        this.int1 = int1;
+    }
+
+    @Column(name = "int2")
+    public Integer getInt2() {
+        return int2;
+    }
+
+    public void setInt2(Integer int2) {
+        this.int2 = int2;
+    }
+
+    @Column(name = "int3")
+    public Integer getInt3() {
+        return int3;
+    }
+
+    public void setInt3(Integer int3) {
+        this.int3 = int3;
+    }
+
+    @Column(name = "int4")
+    public Integer getInt4() {
+        return int4;
+    }
+
+    public void setInt4(Integer int4) {
+        this.int4 = int4;
+    }
+
+    @Column(name = "int5")
+    public Integer getInt5() {
+        return int5;
+    }
+
+    public void setInt5(Integer int5) {
+        this.int5 = int5;
+    }
+
+    @Column(name = "long1")
+    public Long getLong1() {
+        return long1;
+    }
+
+    public void setLong1(Long long1) {
+        this.long1 = long1;
+    }
+
+    @Column(name = "long2")
+    public Long getLong2() {
+        return long2;
+    }
+
+    public void setLong2(Long long2) {
+        this.long2 = long2;
+    }
+
+    @Column(name = "long3")
+    public Long getLong3() {
+        return long3;
+    }
+
+    public void setLong3(Long long3) {
+        this.long3 = long3;
+    }
+
+    @Column(name = "date1")
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    public Date getDate1() {
+        return date1;
+    }
+
+    public void setDate1(Date date1) {
+        this.date1 = date1;
+    }
+
+    @Column(name = "date2")
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    public Date getDate2() {
+        return date2;
+    }
+
+    public void setDate2(Date date2) {
+        this.date2 = date2;
+    }
+
+    @Column(name = "date3")
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    public Date getDate3() {
+        return date3;
+    }
+
+    public void setDate3(Date date3) {
+        this.date3 = date3;
+    }
+
+    @Column(name = "date4")
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    public Date getDate4() {
+        return date4;
+    }
+
+    public void setDate4(Date date4) {
+        this.date4 = date4;
+    }
+
+    @Column(name = "date5")
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    public Date getDate5() {
+        return date5;
+    }
+
+    public void setDate5(Date date5) {
+        this.date5 = date5;
+    }
+
+    @Column(name = "time1")
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    public Date getTime1() {
+        return time1;
+    }
+
+    public void setTime1(Date time1) {
+        this.time1 = time1;
+    }
+
+    @Column(name = "time2")
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    public Date getTime2() {
+        return time2;
+    }
+
+    public void setTime2(Date time2) {
+        this.time2 = time2;
+    }
+
+    @Column(name = "time3")
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    public Date getTime3() {
+        return time3;
+    }
+
+    public void setTime3(Date time3) {
+        this.time3 = time3;
+    }
+
+    @Column(name = "str21",length = 20)
+    public String getStr21() {
+        return str21;
+    }
+
+    public void setStr21(String str21) {
+        this.str21 = str21;
+    }
+
+    @Column(name = "str22",length = 20)
+    public String getStr22() {
+        return str22;
+    }
+
+    public void setStr22(String str22) {
+        this.str22 = str22;
+    }
+
+    @Column(name = "str23",length = 20)
+    public String getStr23() {
+        return str23;
+    }
+
+    public void setStr23(String str23) {
+        this.str23 = str23;
+    }
+
+    @Column(name = "str24",length = 20)
+    public String getStr24() {
+        return str24;
+    }
+
+    public void setStr24(String str24) {
+        this.str24 = str24;
+    }
+
+    @Column(name = "str25",length = 20)
+    public String getStr25() {
+        return str25;
+    }
+
+    public void setStr25(String str25) {
+        this.str25 = str25;
+    }
+
+    @Column(name = "str51", length = 50)
+    public String getStr51() {
+        return str51;
+    }
+
+    public void setStr51(String str51) {
+        this.str51 = str51;
+    }
+
+    @Column(name = "str52",length = 50)
+    public String getStr52() {
+        return str52;
+    }
+
+    public void setStr52(String str52) {
+        this.str52 = str52;
+    }
+
+    @Column(name = "str53",length = 50)
+    public String getStr53() {
+        return str53;
+    }
+
+    public void setStr53(String str53) {
+        this.str53 = str53;
+    }
+
+    @Column(name = "str54",length = 50)
+    public String getStr54() {
+        return str54;
+    }
+
+    public void setStr54(String str54) {
+        this.str54 = str54;
+    }
+
+    @Column(name = "str55",length = 50)
+    public String getStr55() {
+        return str55;
+    }
+
+    public void setStr55(String str55) {
+        this.str55 = str55;
     }
 }
