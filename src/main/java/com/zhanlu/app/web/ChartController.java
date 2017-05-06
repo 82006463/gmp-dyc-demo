822c959462377fd8525e6b4ede71dc75aadf0968
82006463 <82006463@163.com>
2017-05-06 21:39:40 +0800

    aaa

--- src/main/java/com/zhanlu/app/web/ChartController.java
+++ src/main/java/com/zhanlu/app/web/ChartController.java
@@ -6,6 +6,7 @@ import com.zhanlu.framework.nosql.service.MongoService;
 import com.zhanlu.framework.nosql.util.BasicUtils;
 import com.zhanlu.framework.nosql.util.QueryItem;
 import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang.time.DateFormatUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
@@ -16,10 +17,7 @@ import org.springframework.web.servlet.ModelAndView;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
-import java.util.ArrayList;
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
  * 报表Controller
@@ -63,13 +61,39 @@ public class ChartController {
             }
         }
 
+        List<Object> dataResult = new ArrayList<>(1);
+        List<String> categories = new ArrayList<>(10);
+
+        String field1Name = null;
+        String field1Val = null;
+        String field2Name = null;
+        String field2Val = null;
         String selectSql = metaApp.get("selectSql").toString();
         List<Map<String, Object>> selectItems = null;
         if (selectSql.startsWith("SELECT ")) {
             selectItems = jdbcTemplate.queryForList(selectSql);
+        } else if (!selectSql.contains(" FROM ")) {
+            selectItems = new ArrayList<>(12);
+            String[] fieldArr = selectSql.split(",");
+            field1Name = fieldArr[0];
+            field2Name = fieldArr[1];
+            Date curDate = new Date();
+            field1Val = paramMap.get(field1Name) == null || paramMap.get(field1Name)[0].length() == 0 ? DateFormatUtils.format(curDate, "yyyy") + "-01-01" : paramMap.get(field1Name)[0];
+            field2Val = paramMap.get(field2Name) == null || paramMap.get(field2Name)[0].length() == 0 ? DateFormatUtils.format(curDate, "yyyy-MM-dd") : paramMap.get(field2Name)[0];
+            String[] field1Arr = field1Val.split("-");
+            int year = Integer.parseInt(field1Arr[0]);
+            int start = Integer.parseInt(field1Arr[1]);
+            int end = Integer.parseInt(field2Val.split("-")[1]);
+            for (int i = start; i <= end; i++) {
+                Map<String, Object> itemMap = new LinkedHashMap<>(4);
+                String month = i < 10 ? "0" + i : "" + 10;
+                int day = i == 1 || i == 3 || i == 5 || i == 7 || i == 8 || i == 10 || i == 12 ? 31 : i == 4 || i == 6 || i == 9 || i == 11 ? 30 : year % 4 == 0 && year % 100 != 0 ? 29 : 28;
+                itemMap.put("start", field1Arr[0] + "-" + month + "-01");
+                itemMap.put("end", field1Arr[0] + "-" + month + "-" + day);
+                itemMap.put("name", month + "月");
+                selectItems.add(itemMap);
+            }
         }
-        List<Object> dataResult = new ArrayList<>(selectItems.size());
-        List<String> categories = new ArrayList<>(selectItems.size());
         if (selectItems != null && selectItems.size() > 0) {
             String countSql = metaApp.get("countSql").toString();
             String[] fromArr = countSql.split(" FROM ");
@@ -82,15 +106,24 @@ public class ChartController {
                 List<Object> dataList = new ArrayList<>(selectItems.size());
                 for (Map<String, Object> item : selectItems) {
                     categories.add((String) item.get("name"));
-                    List<String> tmpList = jdbcTemplate.queryForList(inSql.replace("?", "'" + item.get("level_no").toString() + "%'"), String.class);
-                    List<String> codeList = new ArrayList<>(tmpList == null ? 1 : tmpList.size() + 1);
-                    codeList.add(item.get("code").toString());
-                    codeList.addAll(tmpList);
                     List<QueryItem> tmpItems = queryItems;
-                    tmpItems.add(new QueryItem(whereArr[1].split("=")[0], codeList));
+                    tmpItems.add(new QueryItem(whereArr[1].split("=")[0], item.get("code")));
                     dataList.add(mongoService.countByProp(whereArr[0], tmpItems));
                 }
                 dataMap.put("name", " ");
+                dataMap.put("data", dataList);
+                dataResult.add(dataMap);
+            } else if (!selectSql.contains(" FROM ")) {
+                Map<String, Object> dataMap = new LinkedHashMap<>(4);
+                List<Object> dataList = new ArrayList<>(selectItems.size());
+                for (Map<String, Object> item : selectItems) {
+                    categories.add((String) item.get("name"));
+                    List<QueryItem> tmpItems = queryItems;
+                    tmpItems.add(new QueryItem(field1Name, item.get("start")));
+                    tmpItems.add(new QueryItem(field2Name, item.get("end")));
+                    dataList.add(mongoService.countByProp(whereArr[0], tmpItems));
+                }
+                dataMap.put("name", field1Val.split("-")[0] + "年");
                 dataMap.put("data", dataList);
                 dataResult.add(dataMap);
             } else {
