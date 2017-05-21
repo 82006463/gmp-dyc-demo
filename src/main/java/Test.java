273b2fa8cd15bf00ee805867f4d3d4c03fd04208
82006463 <82006463@163.com>
2017-05-21 14:12:03 +0800

    aaa

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,37 +1,36 @@
-import com.alibaba.fastjson.JSON;
+import com.mongodb.*;
+import org.springframework.data.mongodb.MongoDbFactory;
+import org.springframework.data.mongodb.core.SimpleMongoDbFactory;
 
 import java.util.List;
-import java.util.Map;
+import java.util.Set;
 
 /**
  * Created by Administrator on 2017/5/1.
  */
 public class Test {
     public static void main(String[] args) {
-        /*String sqlStr = "Like_String_type OR Like_String_name";
+        MongoClient outClient = new MongoClient("101.200.36.213", 27017);
+        MongoDbFactory outFactory = new SimpleMongoDbFactory(outClient, "dyc_uc");
+        DB outDB = outFactory.getDb();
 
-        Map<String, Object> paramMap = JSON.parseObject(sqlStr, Map.class);
+        MongoClient inClient = new MongoClient("127.0.0.1", 27017);
+        MongoDbFactory inFactory = new SimpleMongoDbFactory(inClient, "db_zhanlu");
+        DB inDB = inFactory.getDb();
 
-        MongoClient client = new MongoClient("101.200.36.213", 27017);
-        MongoDbFactory factory = new SimpleMongoDbFactory(client, "dyc_uc");
-        DBCollection app = factory.getDb().getCollection("config_meta_app");
+        Set<String> outNames = outDB.getCollectionNames();
+        for (String outName : outNames) {
+            DBCollection outCollection = outDB.getCollection(outName);
+            DBCursor outCursor = outCollection.find(new BasicDBObject());
+            List<DBObject> outList = outCursor.toArray();
+            outCursor.close();
 
-        DBObject query = new BasicDBObject();
-        query.putAll(paramMap);
-        DBCursor cursor = app.find(query);
-        List<DBObject> list = cursor.toArray();
-        cursor.close();
-
-        System.err.println(JSON.toJSONString(list));
-
-        client.close();*/
-
-        String json = "[{code:'code',name:'name',dataType:'String',tagType:'Input',required:true},{code:'code',name:'name',dataType:'String',tagType:'Input',required:true}]";
-        List<Map<String, Object>> list = JSON.parseObject(json, List.class);
-        for (Map<String,Object> map:list) {
-            for (Map.Entry<String, Object> entry : map.entrySet()) {
-                System.err.println(entry.getKey() + ":" + entry.getValue());
+            DBCollection inCollection = inDB.getCollection(outName);
+            for (DBObject entity : outList) {
+                inCollection.insert(entity);
             }
         }
+        outClient.close();
+        inClient.close();
     }
 }
