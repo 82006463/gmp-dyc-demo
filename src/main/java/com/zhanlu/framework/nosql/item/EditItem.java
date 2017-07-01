c93d92187c06afa8f4f45cbfce43597a6e52b0a3
82006463 <82006463@163.com>
2017-07-01 20:28:35 +0800

    aaa

--- src/main/java/com/zhanlu/framework/nosql/item/EditItem.java
+++ src/main/java/com/zhanlu/framework/nosql/item/EditItem.java
@@ -20,18 +20,31 @@ public class EditItem {
     private String fieldName;
     private Object fieldVal;
 
-    public EditItem(String dataType, String tagType, String attrName, String[] attrValArr) {
+    public EditItem(String dataType, String tagType, String isArray, String attrName, String[] attrValArr) {
         this.fieldName = attrName;
         this.dataType = dataType;
         if (attrValArr != null) {
             if (tagType.equals("subForm")) {
                 fieldVal = attrValArr;
             } else {
-                if (attrValArr[0] != null && attrValArr[0].trim().length() > 0) {
-                    try {
-                        fieldVal = ConvertUtils.convertStringToObject(attrValArr[0], PageEnum.DataType.valueOf(dataType).getClazz());
-                    } catch (Exception e) {
-                        fieldVal = attrValArr;
+                if (dataType.equalsIgnoreCase("String")) {
+                    fieldVal = isArray.equals("1") ? attrValArr : attrValArr[0];
+                } else if (isArray.equals("1")) {
+                    Object[] tmpArr = new Object[attrValArr.length];
+                    for (int i = 0; i < tmpArr.length; i++) {
+                        if (attrValArr[i] == null || attrValArr[i].trim().length() == 0) {
+                            continue;
+                        }
+                        tmpArr[i] = ConvertUtils.convertStringToObject(attrValArr[i], PageEnum.DataType.valueOf(dataType).getClazz());
+                    }
+                    fieldVal = tmpArr;
+                } else {
+                    if (attrValArr[0] != null && attrValArr[0].trim().length() > 0) {
+                        try {
+                            fieldVal = ConvertUtils.convertStringToObject(attrValArr[0], PageEnum.DataType.valueOf(dataType).getClazz());
+                        } catch (Exception e) {
+                            fieldVal = attrValArr;
+                        }
                     }
                 }
             }
@@ -40,16 +53,17 @@ public class EditItem {
         }
     }
 
-    public static EditItem buildEditItem(String dataType, String tagType, String attrName, String[] attrValArr) {
+    public static EditItem buildEditItem(String dataType, String tagType, String isArray, String attrName, String[] attrValArr) {
         if (attrValArr == null || attrValArr[0] == null) {
             return null;
         }
-        return new EditItem(dataType, tagType, attrName, attrValArr);
+        return new EditItem(dataType, tagType, isArray, attrName, attrValArr);
     }
 
     public static List<EditItem> buildEditItems(DataDictService dataDictService, List<Map<String, String>> structList, Map<String, String[]> paramMap) {
         Map<String, String> dataTypeMap = new LinkedHashMap<>(paramMap.size());
         Map<String, String> tagTypeMap = new LinkedHashMap<>(paramMap.size());
+        Map<String, String> arrayMap = new LinkedHashMap<>(paramMap.size());
         for (Map<String, String> struct : structList) {
             String tagType = struct.get("tagType").replace("tagType_", "");
             if (tagType.equals("subForm")) {
@@ -60,14 +74,17 @@ public class EditItem {
                         String[] tmpField = field.split(":");
                         tagTypeMap.put(tmpField[0], "subForm");
                         dataTypeMap.put(tmpField[0], "String");
+                        arrayMap.put(tmpField[0], "1");
                     }
                 }
             } else {
                 tagTypeMap.put(struct.get("code"), tagType);
                 dataTypeMap.put(struct.get("code"), struct.get("dataType").replace("dataType_", ""));
+                arrayMap.put(struct.get("code"), struct.get("array").replace("yesNo_", ""));
                 if (tagType.startsWith("Select")) {
                     tagTypeMap.put(struct.get("code") + "_val", "Input");
                     dataTypeMap.put(struct.get("code") + "_val", "String");
+                    arrayMap.put(struct.get("code") + "_val", arrayMap.get(struct.get("code")));
                 }
             }
         }
@@ -76,9 +93,8 @@ public class EditItem {
             if (!dataTypeMap.containsKey(entry.getKey())) {
                 continue;
             }
-            String tagType = tagTypeMap.get(entry.getKey());
-            String dataType = dataTypeMap.get(entry.getKey());
-            EditItem editItem = EditItem.buildEditItem(dataType, tagType, entry.getKey(), entry.getValue());
+            String key = entry.getKey();
+            EditItem editItem = EditItem.buildEditItem(dataTypeMap.get(key), tagTypeMap.get(key), arrayMap.get(key), key, entry.getValue());
             if (editItem == null) {
                 continue;
             }
